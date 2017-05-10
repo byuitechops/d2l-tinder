280e9c2d18f91da91b467c0c449ff5f075e1488c
Benjamin Earl <benearl72@gmail.com>
2017-05-10 17:39:17 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+var hacker = require('d2l-hacker');
+var output = []
+
+var settings = {
+	waitTimeout: 10 * 60 * 1000
+}
+hacker.run(settings, function (nightmare, data) {
+	return nightmare
+		.goto(`https://${data.domain}.brightspace.com/d2l/le/content/${data.ou}/Home`)
+		.evaluate((callback) => {
+			var sim = $(`<div id="simple" style="position:fixed;font-size:50px;top:200px;left:50px;background-color:#CCC;color:#000;padding:50px">Simple</div>`).appendTo('body').hide()
+			var com = $(`<div id="complex" style="position:fixed;font-size:50px;top:200px;right:50px;background-color:#CCC;color:#000;padding:50px">Complex</div>`).appendTo('body').hide()
+			var whatTheyChose;
+			window.onkeydown = function (e) {
+				switch (e.keyCode) {
+					case 37: // left
+						sim.show()
+						com.hide()
+						whatTheyChose = 'simple'
+						break;
+					case 39: // right
+						sim.hide()
+						com.show()
+						whatTheyChose = 'complex'
+						break;
+					case 13:
+						if (whatTheyChose) {
+							callback(null, whatTheyChose)
+						}
+						break;
+					default:
+						return;
+				}
+				e.preventDefault();
+			}
+		})
+})
+
